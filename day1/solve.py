7350781c04c98091660459b6a18c5618bda88f7c
Omar Abdelbadie <omar.abdelbadie@flexe.com>
2020-12-01 20:03:55 -0800

    second challenge

--- day1/solve.py
+++ day1/solve.py
@@ -1,4 +1,4 @@
-from typing import List, Set, Tuple
+from typing import DefaultDict, Dict, Iterable, List, Set, Tuple
 
 def input_to_list(path: str):
     with open(path, 'r') as f:
@@ -17,6 +17,27 @@ def twoSum(l: List[int], sum: int) -> Tuple[int, int]:
 
 assert(twoSum([-2, 8, 3, 4, 5], 6) == (8,-2))
 
-x1, x2 = twoSum(input_to_list('input.txt'), 2020)
+x1, x2 = twoSum(input_to_list('./input.txt'), 2020)
 answer = x1 * x2
+print("first answer")
 print(answer)
+
+def threeSum(l: List[int], sum: int = 2020) -> Tuple[int, int, int]:
+    d: Dict[int, List[int]] = DefaultDict(lambda: [])
+    for idx,val in enumerate(l):
+        d[sum-val].append(idx)
+    
+    for i in range(len(l)-1):
+        for j in range(i+1, len(l)):
+            x, y = l[i], l[j]
+            z = x+y
+            if z in d:
+                if any([k not in [i,j] for k in d[z]]):
+                    return (x, y, sum-z)
+    raise Exception("no three sum found")
+
+assert(sum(threeSum([3, 4, 5, 8, -9, 4, 5], 4))==4)
+
+a1, a2, a3 = threeSum(input_to_list('./input.txt'), 2020)
+print("second answer")
+print(a1*a2*a3)
